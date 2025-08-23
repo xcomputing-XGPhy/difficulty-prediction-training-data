149922e3dfbd21e0de7bcd50287a928b686bc435
tranthanhvan235 <98211651+tranthanhvan235@users.noreply.github.com>
2025-08-24 02:20:00 +0700

    Update save_best_eval_tree.py
    fix iqtree_parser

--- rules/scripts/save_best_eval_tree.py
+++ rules/scripts/save_best_eval_tree.py
@@ -1,10 +1,10 @@
-from raxmlng_parser import get_all_raxmlng_llhs
+from iqtree_parser import get_all_iqtree_llhs
 from utils import read_file_contents
 
 
 def get_best_tree_and_llh(eval_trees, eval_logs):
     all_trees = read_file_contents(eval_trees)
-    all_llhs = get_all_raxmlng_llhs(eval_logs)
+    all_llhs = get_all_iqtree_llhs(eval_logs)
 
     # get the tree with the highest likelihood
     return max(zip(all_llhs, all_trees), key=lambda x: x[0])
